ac14610b0416a55495268e4fc805365167af1525
Lautaro Conte <info@contelautaro.com.ar>
2024-02-27 00:49:41 -0300

    💄 Cambia UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,26 @@ window.addEventListener("load", function () {
     loader.className += " hidden"; // class "loader hidden"
 });
 
+/*=============== CURSOR ===============*/
+const cursorDot = document.querySelector('[data-cursor-dot]') ;
+const cursorOutline = document.querySelector('[data-cursor-outline]') ;
+
+document.addEventListener('mousemove', (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.top = `${posY}px`;
+    cursorDot.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+    // cursorOutline.style.left = `${posX}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, {duration: 300, fill: "forwards"});
+    });     
+
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
@@ -63,24 +83,24 @@ linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//               sectionTop = current.offsetTop - 58,
+//               sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 
 /*=============== LIGHT DARK THEME ===============*/
@@ -168,7 +188,7 @@ var audioTrigger = document.getElementById("audioTrigger");
     var propuesta = document.getElementById('mensaje').value;
 
     // Crear el enlace de WhatsApp con el mensaje personalizado
-    var mensajeWhatsapp = `Hola mi nombre es ${nombre} y mi mail ${mail}. \n Quiero contactarme con vos para presentarte la siguiente propuesta: \n${propuesta}`;
+    var mensajeWhatsapp = `Hola, mi nombre es *${nombre}* y mi mail *${mail}*\n\nQuiero contactarme con vos para presentarte la siguiente propuesta:\n\n> ${propuesta}`;
     var enlaceWhatsapp = `https://api.whatsapp.com/send?phone=1168851595&text=${encodeURIComponent(mensajeWhatsapp)}`;
 
     // Redirigir a WhatsApp
